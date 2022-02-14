1df4edf88d0e2747694dc2497fb93a75f52d0812
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-02-14 22:58:15 +0900

    loadingの背景色、click to startつけた

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@
 
 //loadingの表示
 $(window).on('load',function(){
-  $(".loading_container").fadeIn(1000);
-  $("#loading").delay(2000).fadeOut('slow');//ローディング画面を2秒（2000ms）待機してからフェードアウト
-  $(".loading_container").delay(1500).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェードアウト
+  $(".loading_container").fadeIn(1000);//loading画面フェードイン
+  $("#loading").click(function(){
+    $("#loading").delay(500).fadeOut('slow');//ローディング画面を2秒（2000ms）待機してからフェードアウト
+  $(".loading_container").fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェードアウト
+  });
+  
 });
