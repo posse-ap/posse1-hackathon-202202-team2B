5543ef2c5eab8e55393b104fd0591f50c5e3dc56
Shun Ninomiya <n119shun@keio.jp>
2022-02-14 23:39:37 +0900

    諦める画面の表示とヒント画面から問題画面に戻る挙動を実装した

--- js/questions.js
+++ js/questions.js
@@ -2,11 +2,25 @@
 
 const questions_main = document.getElementById('main');
 const hint = document.querySelector('.questions_hint');
+const quit = document.querySelector('.questions_quit');
+const try_again = document.querySelector('.hint_try_again');
 
+// ヒント画面の表示
 hint.addEventListener('click', function  display_hint() {
     questions_main.style.display = 'none';
     document.querySelector('.hint_').style.display = 'block';
 })
+// 諦める画面の表示
+quit.addEventListener('click', function  display_quit() {
+    questions_main.style.display = 'none';
+    document.querySelector('.quit_').style.display = 'block';
+})
+
+try_again.addEventListener('click', function close_hint() {
+    questions_main.style.display = 'block';
+    document.querySelector('.hint_').style.display = 'none';
+})
+
 
 const answer = "sample1";
 
