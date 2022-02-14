cb6fd9fd1173c7b825601ef2c2708b25ea9a96fb
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-02-14 23:48:25 +0900

    loadingのアニメーション

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 'use strict';
 
+const CLASSNAME = "-visible";
+const TIMEOUT = 2000;
+const $target = $(".loading_wrapper");
+
 //loadingの表示
-$(window).on('load',function(){
-  $(".loading_container").fadeIn(1000);//loading画面フェードイン
+$(window).on('load',function(){ //ロードされた時
+  $target.delay(2000).addClass(CLASSNAME);
+  
   $("#loading").click(function(){
-    $("#loading").delay(500).fadeOut('slow');//ローディング画面を2秒（2000ms）待機してからフェードアウト
+    setTimeout(() => {
+    $target.removeClass(CLASSNAME);
+  }, TIMEOUT);
+  $("#loading").delay(500).fadeOut('slow');//ローディング画面を2秒（2000ms）待機してからフェードアウト
   $(".loading_container").fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェードアウト
   });
-  
 });
