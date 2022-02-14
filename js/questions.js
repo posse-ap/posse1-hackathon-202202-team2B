29a768df428560df7827492d2c5b20fb92ebbb2b
Shun Ninomiya <n119shun@keio.jp>
2022-02-14 19:25:09 +0900

    ヒントボタン押した時にヒントが表示される画面に切り替わる処理をした

--- js/questions.js
+++ js/questions.js
@@ -3,7 +3,7 @@
 const questions_main = document.getElementById('main');
 const hint = document.querySelector('.questions_hint');
 
-hint.addEventListener('click', function display_hint() {
+hint.addEventListener('click', function  display_hint() {
     questions_main.style.display = 'none';
     document.querySelector('.hint_').style.display = 'block';
 })
