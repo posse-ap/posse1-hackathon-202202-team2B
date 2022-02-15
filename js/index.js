19fa2da086122419d258c5ca55e5d34eab36b31a
Sohtatsu Ido <sohtatsu.ido@keio.jp>
2022-02-15 23:54:51 +0900

    gamestart アニメーションとりあえず

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ const start_wrapper = $(".start_wrapper");
 //loadingの表示
 $(window).on('load',function(){ //ロードされた時
   $target.delay(2000).addClass(CLASSNAME);
+  $('body').removeClass('fadeout');
   start_wrapper.hide();
   setTimeout(function(){
     $("#loading").removeClass('notClick')
@@ -22,3 +23,16 @@ $(window).on('load',function(){ //ロードされた時
   start_wrapper.fadeIn(600);
   });
 });
+
+$(function() {
+  // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
+  $('a:not([href^="#"]):not([target])').on('click', function(e){
+    console.log('aaa');
+    e.preventDefault(); // ナビゲートをキャンセル
+    const url = $(this).attr('href'); // 遷移先のURLを取得
+      $('body').addClass('fadeout');  // bodyに class="fadeout"を挿入
+      setTimeout(function(){
+        window.location = url;  // 0.8秒後に取得したURLに遷移
+      }, 2000);
+  }
+)});
